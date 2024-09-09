afb79ec24569a85ae4fb8662862cf9ab420ae3e3
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-09-09 15:48:46 +0200

    implemented other custom proportions of elements on the screen

--- src/main/java/com/example/currencycalculator/MainScreenController.java
+++ src/main/java/com/example/currencycalculator/MainScreenController.java
@@ -1,9 +1,51 @@
 package com.example.currencycalculator;
 
-public class MainScreenController implements ControllerInterface{
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainScreenController implements ControllerInterface, Initializable {
+    @FXML
+    ChoiceBox sourceCurrencyChoiceBox;
+    @FXML
+    TextField userValueInputTextField;
+    @FXML
+    ChoiceBox resultCurrencyChoiceBox;
+    @FXML
+    TextField resultValueTextField;
+    @FXML
+    Button swapButton;
+
+    @FXML
+    VBox leftVBox;
+
+    @FXML
+    VBox rightVBox;
+
     private LocalConnectionHandler localConnectionHandler;
     @Override
     public void setLocalConnectionHandler(LocalConnectionHandler localConnectionHandler) {
         this.localConnectionHandler = localConnectionHandler;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // ============== setting proportions of ChoiceBoxes and Text fiels to 2:1 ==============
+        //whenever height of container changes, height of nodes also does
+        leftVBox.heightProperty().addListener((observable, odValue, newValue) ->{
+            double containerHeight = newValue.doubleValue();
+            sourceCurrencyChoiceBox.setPrefHeight(containerHeight/3.0);
+            userValueInputTextField.setPrefHeight((containerHeight*2.0)/3.0);
+            resultCurrencyChoiceBox.setPrefHeight(containerHeight/3.0);
+            resultValueTextField.setPrefHeight((containerHeight*2.0)/3.0);
+        });
+
+
     }
 }
